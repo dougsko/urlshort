a28007df1e81326509a10dfa3f59656fb700da92
Doug P <dougtko@gmail.com>
2010-05-19 07:09:22 -0400

    ...

---
+++ lib/urlshort.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+#
+## URL Shortener
+##
+#
+require 'rubygems'
+require 'httpclient'
+
+class URLShort
+    def short
+    end
+end
+
+class Isgd < URLShort
+    def short(link)
+        clnt = HTTPClient.new
+        clnt.get('http://is.gd/api.php', {:longurl => link} ).content
+    end
+end
