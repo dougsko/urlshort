d8c6c8c5df00862a4b5ae7f9f9e5f5bc1062d959
Doug P <dougtko@gmail.com>
2010-05-22 22:23:06 -0400

    spec/urlshort_spec.rb

--- spec/urlshort_spec.rb
+++ spec/urlshort_spec.rb
@@ -2,11 +2,11 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "UrlShort" do
     it "tests is.gd" do
-        UrlShort::IsGd.shorten('google.com').should match "http://is.gd/\w+"
+        UrlShort::IsGd.shorten('google.com').should match /http:\/\/is\.gd\/\w+/
     end
 
     it "tests TinyUrl" do
-        UrlShort::TinyUrl.shorten('google.com').should match "http://tinyurl.com/\w+"
+        UrlShort::TinyUrl.shorten('google.com').should match /http:\/\/tinyurl\.com\/\w+/
     end
 
 end
