b4d05524fcc870a6a82eaa5b37f9dbbf5a0d8af6
Doug P <dougtko@gmail.com>
2012-09-29 13:14:32 -0400

    ...

--- Rakefile
+++ Rakefile
@@ -1,41 +1,44 @@
+# encoding: utf-8
+
 require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
 require 'rake'
 
-begin
-  require 'jeweler'
-  Jeweler::Tasks.new do |gem|
-    gem.name = "urlshort"
-    gem.summary = %Q{Makes URLs short using services like is.gd and TinyUrl.}
-    gem.description = %Q{Makes URLs short using services like is.gd and TinyUrl.}
-    gem.email = "dougtko@gmail.com"
-    gem.homepage = "http://github.com/dougsko/urlshort"
-    gem.authors = ["Doug P."]
-    gem.add_development_dependency "rspec"
-    gem.add_dependency "httpclient"
-    # gem is a Gem::Specification... see http://www.rubygems.org/read/chapter/20 for additional settings
-  end
-  Jeweler::GemcutterTasks.new
-rescue LoadError
-  puts "Jeweler (or a dependency) not available. Install it with: gem install jeweler"
+require 'jeweler'
+Jeweler::Tasks.new do |gem|
+  # gem is a Gem::Specification... see http://docs.rubygems.org/read/chapter/20 for more options
+  gem.name = "urlshort"
+  gem.homepage = "http://github.com/dougsko/urlshort"
+  gem.license = "MIT"
+  gem.summary = %Q{TODO: one-line summary of your gem}
+  gem.description = %Q{TODO: longer description of your gem}
+  gem.email = "dougtko@gmail.com"
+  gem.authors = ["Doug P."]
+  # dependencies defined in Gemfile
+end
+Jeweler::RubygemsDotOrgTasks.new
+
+require 'rspec/core'
+require 'rspec/core/rake_task'
+RSpec::Core::RakeTask.new(:spec) do |spec|
+  spec.pattern = FileList['spec/**/*_spec.rb']
 end
 
-require 'spec/rake/spectask'
-Spec::Rake::SpecTask.new(:spec) do |spec|
-  spec.libs << 'lib' << 'spec'
-  spec.spec_files = FileList['spec/**/*_spec.rb']
-end
-
-Spec::Rake::SpecTask.new(:rcov) do |spec|
-  spec.libs << 'lib' << 'spec'
+RSpec::Core::RakeTask.new(:rcov) do |spec|
   spec.pattern = 'spec/**/*_spec.rb'
   spec.rcov = true
 end
 
-task :spec => :check_dependencies
-
 task :default => :spec
 
-require 'rake/rdoctask'
+require 'rdoc/task'
 Rake::RDocTask.new do |rdoc|
   version = File.exist?('VERSION') ? File.read('VERSION') : ""
 
--- lib/urlshort.rb
+++ lib/urlshort.rb
@@ -3,24 +3,97 @@
 ## URL Shortener
 ##
 #
-require 'rubygems'
+
 require 'httpclient'
 
-module UrlShort
-
-    class IsGd
-        def self.shorten(link)
-            clnt = HTTPClient.new
-            clnt.get('http://is.gd/api.php', {:longurl => link} ).content
-        end
+class BadRequest < RuntimeError
+    def initialize(msg)
+        puts "There was a problem with the original long URL provided."
+        puts msg
     end
+end
 
-    class TinyUrl
-        def self.shorten(link)
-            clnt = HTTPClient.new
-            clnt.get('http://tinyurl.com/api-create.php', {:url => link} ).content
-        end
+class NotAcceptable < RuntimeError
+    def initialize(msg)
+        puts "There was a problem with the short URL provided."
+        puts msg
     end
+end
 
+class BadGateway < RuntimeError
+    def initialize(msg)
+        puts "Our rate limit was exceeded."
+        puts msg
+    end
+end
+    
+class ServiceUnavailable < RuntimeError
+    def initialize(msg)
+        puts "This service is temporarily unavailable."
+        puts msg
+    end
+end
 
+class IsGd
+    def self.shorten(link)
+        err = 0
+        clnt = HTTPClient.new
+        resp = clnt.get('http://is.gd/create.php', 
+                 {:format => 'simple', :url => link} )#.content
+        begin
+            case resp.status_code
+            when 200
+                return resp.content
+            when 400
+                raise BadRequest.new(resp.content)
+            when 406
+                raise NotAcceptable.new(resp.content)
+            when 502
+                raise BadGateway.new(resp.content)
+            when 503
+                raise ServiceUnavailable.new(resp.content)
+            end
+        rescue BadRequest
+            err = 1
+        rescue NotAcceptable
+            err = 1
+        rescue BadGateway
+            err = 1
+        rescue ServiceUnavailable
+            err = 1
+        end
+        return err
+    end
+end
+
+class VGd
+    def self.shorten(link)
+        err = 0
+        clnt = HTTPClient.new
+        resp = clnt.get('http://v.gd/create.php', 
+                 {:format => 'simple', :url => link} )#.content
+        begin
+            case resp.status_code
+            when 200
+                return resp.content
+            when 400
+                raise BadRequest.new(resp.content)
+            when 406
+                raise NotAcceptable.new(resp.content)
+            when 502
+                raise BadGateway.new(resp.content)
+            when 503
+                raise ServiceUnavailable.new(resp.content)
+            end
+        rescue BadRequest
+            err = 1
+        rescue NotAcceptable
+            err = 1
+        rescue BadGateway
+            err = 1
+        rescue ServiceUnavailable
+            err = 1
+        end
+        return err
+    end
 end
